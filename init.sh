bc9ae1acbe6ebc1e754ebb5f9a614f1ed2e0a23f
PhucPear <huynhphuc7012@gmail.com>
2026-04-10 22:49:58 +0700

    Config docker

---
+++ init.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+ENV=${1:-dev}
+
+# màu mè chút cho ngầu 😎
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # no color
+
+print_line() {
+  echo -e "${YELLOW}=====================================${NC}"
+}
+
+print_title() {
+  echo -e "${GREEN}$1${NC}"
+}
+
+print_step() {
+  echo -e "${YELLOW}➜ $1...${NC}"
+}
+
+print_success() {
+  echo -e "${GREEN}✔ $1${NC}"
+}
+
+print_error() {
+  echo -e "${RED}✖ $1${NC}"
+}
+
+# header
+echo ""
+print_line
+print_title "🚀 STARTING SETUP ($ENV)"
+print_line
+echo ""
+
+# chọn môi trường
+if [ "$ENV" = "dev" ]; then
+  COMPOSE_FILE=docker-compose.dev.yml
+elif [ "$ENV" = "production" ]; then
+  COMPOSE_FILE=docker-compose.prod.yml
+else
+  print_error "Invalid environment. Use dev or prod"
+  exit 1
+fi
+
+# chạy docker
+print_step "Building & starting containers"
+docker-compose -f $COMPOSE_FILE up -d --build
+
+print_step "Waiting for app to boot"
+sleep 10
+
+# setup laravel
+print_step "Installing dependencies & setup Laravel"
+docker exec hotel_booking_app bash -c "
+composer install &&
+php artisan key:generate
+"
+
+# dev vs prod
+if [ "$ENV" = "dev" ]; then
+  print_step "Running migrations DEV only"
+  docker exec hotel_booking_app php artisan migrate
+fi
+
+if [ "$ENV" = "production" ]; then
+  print_step "Optimizing Laravel (PROD)"
+  docker exec hotel_booking_app bash -c "
+  php artisan config:cache &&
+  php artisan route:cache &&
+  php artisan view:cache
+  "
+fi
+
+# done
+echo ""
+print_line
+print_success "🎉 Setup completed for $ENV!"
+print_line
+echo ""
